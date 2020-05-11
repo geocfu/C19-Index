7f0ae14c9760002dd47e5db423639e399289bb64
George Mantellos <geocfu@tutamail.com>
2020-05-12 00:16:00 +0300

    Code cleanup on Settings screen

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { SafeAreaView, Linking, View, StyleSheet } from "react-native";
+import { SafeAreaView, Linking, View, ScrollView } from "react-native";
 import {
   Icon,
   Layout,
   Text,
   TopNavigation,
   TopNavigationAction,
-  Button,
   ListItem,
 } from "@ui-kitten/components";
 import Constants from "expo-constants";
@@ -40,106 +39,101 @@ const Settings = ({ navigation }) => {
         accessoryLeft={renderBackAction}
       />
       <Layout style={{ flex: 1 }} level="1">
-        <View
-          style={{
-            marginLeft: 10,
-            marginRight: 10,
-            marginTop: 10,
-            marginBottom: 10,
-          }}
-        >
-          <Text
-            category="h5"
+        <ScrollView>
+          <View
             style={{
-              fontWeight: "bold",
+              marginLeft: 10,
+              marginRight: 10,
+              marginTop: 10,
+              marginBottom: 10,
             }}
           >
-            Themes
-          </Text>
+            <Text
+              category="h5"
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              Themes
+            </Text>
 
-          <View style={{ flexDirection: "column" }}>
-            <ThemeCard
-              onPress={() => {
-                if (themeContext.theme === "dark") {
-                  themeContext.toggleTheme();
-                }
+            <View style={{ flexDirection: "column" }}>
+              <ThemeCard
+                onPress={() => {
+                  if (themeContext.theme === "dark") {
+                    themeContext.toggleTheme();
+                  }
+                }}
+                title="Light"
+              />
+              <ThemeCard
+                onPress={() => {
+                  if (themeContext.theme === "light") {
+                    themeContext.toggleTheme();
+                  }
+                }}
+                title="Dark"
+              />
+            </View>
+            <Text
+              category="h5"
+              style={{
+                fontWeight: "bold",
+                marginTop: 10,
               }}
-              title="Light"
+            >
+              About
+            </Text>
+
+            <ListItem
+              style={{ borderRadius: 5 }}
+              title={
+                <Text category="h6" appearance="hint">
+                  Version
+                </Text>
+              }
+              description={
+                <Text>
+                  {Constants.manifest.version} ({Constants.nativeBuildVersion})
+                </Text>
+              }
+              accessoryLeft={VersionIcon}
+              disabled
             />
-            <ThemeCard
+            <ListItem
+              style={{ borderRadius: 5 }}
+              title={<Text category="h6">Report an issue</Text>}
+              description={
+                <Text appearance="hint">Having an issue? Report it here</Text>
+              }
+              accessoryLeft={BugIcon}
               onPress={() => {
-                if (themeContext.theme === "light") {
-                  themeContext.toggleTheme();
-                }
+                Linking.openURL("https://github.com/geocfu/C19-Index/issues");
               }}
-              title="Dark"
+            />
+            <ListItem
+              style={{ borderRadius: 5 }}
+              title={<Text category="h6">BiHELab</Text>}
+              description={<Text appearance="hint">Organizer</Text>}
+              accessoryLeft={ExternalLinkIcon}
+              onPress={() => {
+                Linking.openURL("http://bihelab.di.ionio.gr/");
+              }}
+            />
+            <ListItem
+              style={{ borderRadius: 5 }}
+              title={<Text category="h6">George Mantellos</Text>}
+              description={<Text appearance="hint">App Developer</Text>}
+              accessoryLeft={ExternalLinkIcon}
+              onPress={() => {
+                Linking.openURL("https://github.com/geocfu/");
+              }}
             />
           </View>
-          <Text
-            category="h5"
-            style={{
-              fontWeight: "bold",
-              marginTop: 10,
-            }}
-          >
-            About
-          </Text>
-
-          <ListItem
-            style={{ borderRadius: 5 }}
-            title={
-              <Text category="h6" appearance="hint">
-                Version
-              </Text>
-            }
-            description={
-              <Text>
-                {Constants.manifest.version} ({Constants.nativeBuildVersion})
-              </Text>
-            }
-            accessoryLeft={VersionIcon}
-            disabled
-          />
-          <ListItem
-            style={{ borderRadius: 5 }}
-            title={<Text category="h6">Report an issue</Text>}
-            description={
-              <Text appearance="hint">Having an issue? Report it here</Text>
-            }
-            accessoryLeft={BugIcon}
-            onPress={() => {
-              Linking.openURL("https://github.com/geocfu/C19-Index/issues");
-            }}
-          />
-          <ListItem
-            style={{ borderRadius: 5 }}
-            title={<Text category="h6">BiHELab</Text>}
-            description={<Text appearance="hint">Organizer</Text>}
-            accessoryLeft={ExternalLinkIcon}
-            onPress={() => {
-              Linking.openURL("http://bihelab.di.ionio.gr/");
-            }}
-          />
-          <ListItem
-            style={{ borderRadius: 5 }}
-            title={<Text category="h6">George Mantellos</Text>}
-            description={<Text appearance="hint">App Developer</Text>}
-            accessoryLeft={ExternalLinkIcon}
-            onPress={() => {
-              Linking.openURL("https://github.com/geocfu/");
-            }}
-          />
-        </View>
+        </ScrollView>
       </Layout>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  aboutButton: {
-    justifyContent: "space-between",
-    marginTop: 10,
-  },
-});
 
 export default Settings;
