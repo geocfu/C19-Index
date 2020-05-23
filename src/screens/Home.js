e4f490eb16617cd679a0734b3a8cd751b57161a9
George Mantellos <geocfu@tutamail.com>
2020-05-23 18:57:38 +0300

    Finalized Home design

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, View, ScrollView } from "react-native";
+import { SafeAreaView, View, ScrollView, Image } from "react-native";
 import {
   Layout,
   TopNavigation,
@@ -7,13 +7,24 @@ import {
   Icon,
   Text,
   Button,
-  Card,
+  useTheme,
 } from "@ui-kitten/components";
+
+import SurgicalMaskImage from "../components/svgs/SurgicalMaskImage";
+
+import VirusImage from "../components/svgs/VirusImage.js";
+import Virus1Image from "../components/svgs/Virus1Image.js";
+import Virus3Image from "../components/svgs/Virus3Image.js";
+import Virus4Image from "../components/svgs/Virus4Image.js";
+import Virus2Image from "../components/svgs/Virus2Image.js";
+import Virus5Image from "../components/svgs/Virus5Image.js";
 
 const SettingsIcon = (props) => <Icon {...props} name="settings-outline" />;
 const ForwardIcon = (props) => <Icon {...props} name="arrow-forward" />;
 
 const Home = ({ navigation }) => {
+  const theme = useTheme();
+
   const renderSettingsAction = () => (
     <TopNavigationAction
       icon={SettingsIcon}
@@ -22,7 +33,7 @@ const Home = ({ navigation }) => {
       }}
     />
   );
-
+  //@refresh reset
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation accessoryRight={renderSettingsAction} />
@@ -30,6 +41,8 @@ const Home = ({ navigation }) => {
         <ScrollView>
           <View
             style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
               marginLeft: 10,
               marginRight: 10,
               marginTop: 10,
@@ -46,6 +59,25 @@ const Home = ({ navigation }) => {
               {"\n"}
               Stay Safe
             </Text>
+            <SurgicalMaskImage width="100" height="100" />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              marginLeft: 10,
+              marginRight: 10,
+              marginTop: 10,
+              marginBottom: 10,
+              zIndex: -10,
+            }}
+          >
+            <VirusImage width="50" height="50" />
+            <Virus1Image width="50" height="50" />
+            <Virus2Image width="50" height="50" />
+            <Virus3Image width="50" height="50" />
+            <Virus4Image width="50" height="50" />
+            <Virus5Image width="50" height="50" />
           </View>
         </ScrollView>
         <View
